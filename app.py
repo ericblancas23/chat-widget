90e1691ff19ec687329c9e55328f8df7dd50f061
EricB <ericblancas23@gmail.com>
2018-09-30 19:51:54 -0700

    finshed backend testing

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify, makeresponse, json
 from flaskcors import CORS
 from pusher import pusher
 import simplejson
 
-app = Flask(__**name**__)
+app = Flask(_**name**_)
+
 cors = CORS(app)
-app.config_[_'CORSHEADERS'] =  'Content-Type'
+
+app.config_[_'CORSHEADERS'] = 'Content-Type'
 
 pusher = pusher.Pusher(
     app_id = 'PUSHER_APP_ID'
@@ -38,4 +40,12 @@ def guestUser():
     })
 
     return json.dumps(data)
+
+@app.route("/pusher/auth", methods=_[_'POST'])
+def pusher_authentication():
+    auth = pusher.authentication(channel=request.form_[_'channel-name'], socket_id=request.form_[_'socket_id'])
+    return json.dump(auth)
+
+if _**name == '**_main_':
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
