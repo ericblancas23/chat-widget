b170c662c034c4afbe3326225b33fbb36fbfc478
EricB <ericblancas23@gmail.com>
2018-10-06 16:24:50 -0700

    created helper functions

---
+++ client/src/components/Chat.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import * as Pusher from 'pusher';
+
+
+export class Chat extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: undefined,
+            email: undefined,
+            myChannel: undefined
+        }
+
+        const pusher = new Pusher('PUSHER_APP_KEY', {
+            authEndpoint: '',
+            cluster: 'PUSHER_APP_CLUSTER',
+            encrypted: true
+        })
+
+        const chatPage = $(document)
+        const chatWindow = $('.chatbubble')
+        const chatHeader = chatWindow.find('.unexpanded')
+        const chatBody = chatWindow.find('.chat-window')
+
+        this.toggle = this.toggle.bind
+        this.showAppropriateChatDisplay = this.showAppropriateChatDisplay.bind
+        this.showInitiationDisplay = this.showInitiationDisplay.bind
+        this.showChatRoomDisplay = this.showChatRoomDisplay.bind
+        
+    }
+}
